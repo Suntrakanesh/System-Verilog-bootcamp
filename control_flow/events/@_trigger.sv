bf271009ca3dc235e932b471275e0dfa5da08bcc
Shakthi priya <75575691+Astronaut2002shakthi@users.noreply.github.com>
2021-09-26 16:56:44 +0530

    Create @_trigger.sv
    Code for the race condition of @ and trigger events

---
+++ control_flow/events/@_trigger.sv
@@ -0,0 +1,17 @@
+module events;
+  event name; //event is used for the processes to be trigger and wait
+  initial begin
+    #20 -> name;
+    $display("The event 1 is triggered");
+  end
+  initial begin
+    $display("The event 2 is waiting for triggering");
+    #20 @(name);
+    $display("The event 2 received trigger");
+  end
+  initial begin
+    $display("The event 3 is waiting for triggering");
+    #20 wait(name.triggered);
+    $display("The event 3 received trigger");
+  end
+endmodule
