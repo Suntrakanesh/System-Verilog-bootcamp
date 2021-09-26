0705dae08908e920a6889105524e9aeb0982930b
Shakthi priya <75575691+Astronaut2002shakthi@users.noreply.github.com>
2021-09-26 17:28:25 +0530

    Create blocking.sv
    Code for blocking assignments

---
+++ control_flow/blocking&non_blocking/blocking.sv
@@ -0,0 +1,19 @@
+module blocking;
+  bit [7:0] a,b,c,d,e;
+  initial begin
+    a = 8'hab;
+    $display("[%t] a = %b; b = %b; c = %b",$time,a,b,c);
+    b = 8'hbc;
+     $display("[%t] a = %b; b = %b; c = %b",$time,a,b,c);
+    c = 8'hca;
+     $display("[%t] a = %b; b = %b; c = %b",$time,a,b,c);
+  end
+  
+  initial begin
+    d = 8'hde;
+    $display("[%t] d = %b; e = %b",$time,d,e);
+    e = 8'hef;
+     $display("[%t] d = %b; e = %b",$time,d,e);
+  end
+endmodule
+    
