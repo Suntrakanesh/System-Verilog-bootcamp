a583c62b2ec8313369561eb109912e1d3fa9cdbc
Shakthi priya <75575691+Astronaut2002shakthi@users.noreply.github.com>
2021-09-28 13:02:46 +0530

    Create task_using.sv
    Code for using task in fork join program

---
+++ processes/fork_join/task_using.sv
@@ -0,0 +1,15 @@
+module fork_join;
+  initial begin
+    $display("[%t] Main thread: Fork join going to start",$time);
+    fork
+      print (40,"Thread 0");
+      print (30, "Thread 1");
+    join_none
+    $display("[%t] Main thread: Fork join comes to end",$time);
+  end
+    
+  task  print (int _time, string tm_str);
+      #(_time) $display("[%t] %s",$time,tm_str);
+    endtask
+  
+endmodule
