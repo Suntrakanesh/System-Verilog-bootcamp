345aaf7438ceb23278d400d41da151979cc39977
Shakthi priya <75575691+Astronaut2002shakthi@users.noreply.github.com>
2021-09-28 11:24:59 +0530

    Update nested fork_join.sv

--- processes/fork_join/nested fork_join.sv
+++ processes/fork_join/nested fork_join.sv
@@ -2,12 +2,12 @@ module fork_join;
   initial begin
     $display("[%t] Main thread: Fork join going to start",$time);
     fork
-      #2 $display("[%t] Thread 1 is executing",$time);
-      #4 $display("[%t] Thread 1 is executed",$time);
+       $display("[%t] Thread 1 is executing",$time);
+       $display("[%t] Thread 1 is executed",$time);
       fork
-        #15 $display("[%t] The nested loop is running",$time);
+        #5 $display("[%t] The nested loop is running",$time);
         begin
-          #5 $display("[%t] The nested loop is executed and fininshed",$time);
+          #15 $display("[%t] The nested loop is executed and fininshed",$time);
         end
       join
       #20 $display("[%t] Nested loop fork join comes to end",$time);
