9396bcc4f22d3bb02a76614c8e53940533a24046
Shakthi priya <75575691+Astronaut2002shakthi@users.noreply.github.com>
2021-09-28 13:24:20 +0530

    Create wait_fork.sv
    Code for wait fork program

---
+++ processes/fork_join/wait_fork.sv
@@ -0,0 +1,17 @@
+module fork_join;
+  initial begin
+    $display("[%t] Main thread: Fork join going to start",$time);
+    fork
+      print (40,"Thread 0");
+      print (30, "Thread 1");
+    join_any
+    $display("[%t] Main thread: Fork join comes to end",$time);
+    
+    wait fork;
+  end
+  
+  task automatic print (int _time, string tm_str);
+      #(_time) $display("[%t] %s",$time,tm_str);
+    endtask
+  
+endmodule
