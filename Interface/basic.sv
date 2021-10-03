4f59b0f207385bd9dcc2784e2c03033b1ddb038b
Shakthi priya <75575691+Astronaut2002shakthi@users.noreply.github.com>
2021-10-03 15:48:34 +0530

    Create basic.sv
    Code for basic interface program

---
+++ Interface/basic.sv
@@ -0,0 +1,32 @@
+interface signal(input clk);
+  logic data;
+  logic enable;
+  modport TB(input data,clk, output enable);
+  modport DUT(input clk,enable, output data);
+endinterface
+
+module tb;
+  bit clk;
+  always #10 clk = ~clk;
+  signal fact(clk);       //Create an interface object
+  face dut0(fact.DUT);    //Instantiate DUT
+  initial begin
+    fact.enable <= 0;
+    $display("[%t] The outcome is ",$time,fact.enable);
+    #10 fact.enable <= 1;
+    $display("[%t] The outcome is ",$time,fact.enable);
+    #40 fact.enable <= 0;
+    $display("[%t] The outcome is ",$time,fact.enable);
+    #20 fact.enable <= 1;
+    $display("[%t] The outcome is ",$time,fact.enable);
+    #100 $finish;
+  end
+endmodule
+// Code your design here
+module face(signal fact);
+  always @(posedge fact.clk)
+      if (fact.enable)
+        fact.data <= fact.data+1;
+      else
+        fact.data <= 0; 
+endmodule
