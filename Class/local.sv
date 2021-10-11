2181780604a934d0b1e306cbe75be573364df522
Shakthi priya <75575691+Astronaut2002shakthi@users.noreply.github.com>
2021-10-11 16:14:27 +0530

    Update local.sv

--- Class/local.sv
+++ Class/local.sv
@@ -12,7 +12,7 @@ module main;
   locals fun = new;
   initial begin
     fun.display(25,'b111);
-    //$display("The value of data = %d and value = %b",fun.data,fun.value) [Here, data variable value cannot be accessed outside the class due to the local keyword before it]
+    //$display("The value of data = %d and value = %b",fun.data,fun.value) [Here, data variable value cannot be accessed outside the class due to the local keyword before it] and evenif parent class has local variable associated with it,it is not visible to child class and it cannot be accessed from the child class 
     $display("The value of data = %d",fun.data);
   end
 endmodule
