ea73cd8fac3bec50175f9ba5af218692b11ee1e2
Shakthi priya <75575691+Astronaut2002shakthi@users.noreply.github.com>
2021-10-16 17:29:07 +0530

    Create inside.sv
    Code for inside keyword

---
+++ constraits/inside.sv
@@ -0,0 +1,16 @@
+class inside_fn;
+  rand bit [1:0] data;
+  rand bit [3:0] val;
+  constraint con{ data inside{[3:7]};val inside{4,5};}
+endclass
+
+module main;
+  initial begin
+  inside_fn fun = new;
+  fun.randomize();
+  $display("The value of data = %p and value = %p",fun.data,fun.val);
+  end
+endmodule
+  
+  
+  
