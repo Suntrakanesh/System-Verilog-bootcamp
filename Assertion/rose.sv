9a769b0253abfd024b94fb55981b1f39b929bb2c
Suntrakanesh SU <64604283+Suntrakanesh@users.noreply.github.com>
2021-11-01 13:23:43 +0530

    Create rose.sv

---
+++ Assertion/rose.sv
@@ -0,0 +1,20 @@
+module tb;
+  bit a, clk;
+  always #5 clk = ~clk;
+  sequence ass;
+    @(posedge clk) $rose(a);
+  endsequence
+  always #10 a = ~a;
+    
+  initial begin
+    $dumpvars;
+    $dumpfile("assert.vcd");
+  end
+  
+  assert property (ass);  
+    
+  initial begin
+    #200 $finish;
+  end
+    
+endmodule
